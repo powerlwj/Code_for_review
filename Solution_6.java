31ce3f71fc9cf1587cd13fe3519744a8570c3f11
powerlwj <569546435@qq.com>
2019-04-29 12:28:32 +0800

    Update Solution_6.java

--- Solution_6.java
+++ Solution_6.java
@@ -1,4 +1,4 @@
-import static java.util.Arrays.copyOfRange;
+import java.util.*;
 
 public class Solution_6 {
     private class TreeNode {
@@ -15,24 +15,19 @@ public class Solution_6 {
         s6.reConstructBinaryTree(pre,in);
      }
 
-     public TreeNode reConstructBinaryTree(int[] pre,int[] in){
-        if (pre==null || in==null) return null;
-        if (pre.length==in.length && pre.length==0) return null;
-        TreeNode treeNode=new TreeNode(pre[0]);
-        //下面这个循环是为了递归分开左子树和右子树
-         for (int i = 0; i <pre.length&&in[i]!=pre[0] ; i++) {
-            if (i>=1){
-                treeNode.left=reConstructBinaryTree(copyOfRange(pre,1,i+1),copyOfRange(in,0,i));
-            }else {
-                treeNode.left=null;
+
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+       if(pre.length == 0||in.length == 0){
+            return null;
+        }
+        TreeNode node = new TreeNode(pre[0]);
+        for(int i = 0; i < in.length; i++){
+            if(pre[0] == in[i]){
+                node.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i+1), Arrays.copyOfRange(in, 0, i));
+                node.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i+1, pre.length), Arrays.copyOfRange(in, i+1,in.length));
             }
-            if (i+1<pre.length){
-                treeNode.right=reConstructBinaryTree(copyOfRange(pre,i+1,pre.length),copyOfRange(in,i+1,in.length));
-            }else {
-                treeNode.right=null;
-            }
-         }
-         return treeNode;
-     }
+        }
+        return node;
+    }
 
 }
